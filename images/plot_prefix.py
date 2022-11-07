71360f848abe02b9a200c3de012f598dafed8aeb
Maximilian Schattauer <maximilian.schattauer@mpq.mpg.de>
2022-11-07 18:46:32 +0100

    Added field measurements

---
+++ images/plot_prefix.py
@@ -0,0 +1,9 @@
+cm = 1/2.54
+thesis_red = "#FF0000"
+thesis_gray = "#474847"
+thesis_rebecca = "#693EA3"
+thesis_fuchsia = "#9A5B91"
+thesis_blue = "#50BDE9"
+plt.rcParams['axes.prop_cycle'] = plt.cycler(color=[thesis_red, thesis_blue, thesis_rebecca, thesis_gray, thesis_fuchsia])
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.size'] = 9
