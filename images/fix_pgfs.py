a64268f27ba2d4b2374276b50c9886943e492b6c
Maximilian Schattauer <maximilian.schattauer@mpq.mpg.de>
2022-11-21 22:30:47 +0100

    Created script for pgf math mode correction

---
+++ images/fix_pgfs.py
@@ -0,0 +1,26 @@
+"""This scripts fixes the math environment error in pgfs output by matplotlib
+
+\pgftext[x=2.702656in,y=0.397499in,,top]{\color{textcolor}\rmfamily\fontsize{9.000000}{10.800000}\selectfont 0.25}%
+
+"""
+import sys
+import re
+
+filenames = sys.argv[1:]
+
+regex = r'^(?P<start>\\pgftext\[.*\]\{\\color{textcolor}\\rmfamily\\fontsize{9.000000}{10.800000}\\selectfont\s+)(?P<number>\d+(\.\d+)?)\}'
+
+if __name__ == '__main__':
+    for filename in filenames:
+        lines = []
+        output_lines = []
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            match = re.match(regex, line)
+            if match:
+                line = re.sub(regex, r'\g<start>\\ensuremath{\g<number>}}', line)
+            output_lines.append(line)
+            print(line, end='')
+            
+    
