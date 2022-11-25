c60b9f240bba05740994ad84d3415d19126f97e3
Maximilian Schattauer <max@schattauer.info>
2022-11-25 01:49:26 +0100

    Added watermark and changed low quality filename

--- text/99-total/to_low_quality.sh
+++ text/99-total/to_low_quality.sh
@@ -1,14 +1,16 @@
-ps2pdf -dPDFSETTINGS=/ebook output/99-total.pdf lq.pdf
+TARGET_FILENAME="schattauer_msc_lq.pdf"
+
+ps2pdf -dPDFSETTINGS=/ebook output/99-total.pdf "$TARGET_FILENAME"
 
 MPQ_RZG_PATH="/home/max/Schattauer/DataShare"
 HOME_RZG_PATH="/home/maxi/Masterarbeit"
 if [ -d "$MPQ_RZG_PATH" ]
 then
-	cp lq.pdf "$MPQ_RZG_PATH"
+	cp "$TARGET_FILENAME" "$MPQ_RZG_PATH"
 	echo "Copied to $MPQ_RZG_PATH" >&2
 fi
 if [ -d "$HOME_RZG_PATH" ]
 then
-	cp lq.pdf "$HOME_RZG_PATH"
+	cp "$TARGET_FILENAME" "$HOME_RZG_PATH"
 	echo "Copied to $HOME_RZG_PATH" >&2
 fi
